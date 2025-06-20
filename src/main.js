28e00a64b4d7152b38f913809e8ee12ef22cf915
ThuyTien0707 <tranthithuytien070705@gmail.com>
2025-06-20 23:22:25 +0700

    .

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 // import bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
+
 // import bootstrap JS nếu bạn dùng tooltip/modal...
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
