40fb391df7b9cb1def6ede52800db4ff5f00fd1a
HanderJacier <bang867666@gmail.com>
2025-07-27 21:57:04 +0700

    thêm link ảnh

--- src/components/Admin/QLSanPham/Form.vue
+++ src/components/Admin/QLSanPham/Form.vue
@@ -109,50 +109,49 @@ const loaiOptions = computed(() =>
       <div class="col-md-4" v-for="key in visibleFields" :key="key">
         <label class="form-label">{{ formFields[key] }}</label>
         <!-- Ảnh gốc -->
-        <input v-if="key === 'anhgoc'"
-          type="file" class="form-control" @change="onImageChange" />
+        <div v-if="key === 'anhgoc'">
+          <input type="file" class="form-control" @change="onImageChange" />
+          <!-- Hiện tên file hoặc link nếu đang sửa và có ảnh -->
+          <div v-if="isEditing && (productForm.anhgoc || productForm.diachianh)" class="mt-1 small text-secondary">
+            <span v-if="productForm.diachianh && productForm.diachianh.startsWith('blob:')">
+              {{ productForm.diachianh.split('/').pop() }}
+            </span>
+            <span v-else-if="productForm.anhgoc">
+              <a :href="productForm.anhgoc" target="_blank">{{ productForm.anhgoc.split('/').pop() }}</a>
+            </span>
+          </div>
+        </div>
         <!-- Ảnh phụ -->
         <div v-else-if="key === 'anhphu'">
           <input type="file" class="form-control" multiple @change="onAnhPhuChange" />
           <div class="mt-2 d-flex flex-wrap gap-2">
-            <img v-for="(src, idx) in previewAnhPhu" :key="idx" :src="src" width="60" height="60" class="rounded border" />
+            <img v-for="(src, idx) in previewAnhPhu" :key="idx" :src="src" width="60" height="60"
+              class="rounded border" />
           </div>
         </div>
         <!-- Các input khác -->
-        <input v-else
-          :type="['dongia', 'soluong'].includes(key) ? 'number' : 'text'" class="form-control"
+        <input v-else :type="['dongia', 'soluong'].includes(key) ? 'number' : 'text'" class="form-control"
           v-model="productForm[key]" />
       </div>
 
       <!-- Preview ảnh chính -->
-<div class="col-md-4" v-if="productForm.diachianh || productForm.anhgoc">
-  <label class="form-label d-block">Xem trước ảnh chính</label>
-  <!-- Nếu vừa chọn file mới (blob), ưu tiên hiện -->
-  <img
-    v-if="productForm.diachianh && productForm.diachianh.startsWith('blob:')"
-    :src="productForm.diachianh"
-    width="100"
-    height="100"
-    class="rounded"
-  />
-  <!-- Nếu đã upload lên cloud hoặc load lại (link cloud) -->
-  <img
-    v-else-if="productForm.anhgoc"
-    :src="productForm.anhgoc"
-    width="100"
-    height="100"
-    class="rounded"
-  />
-</div>
+      <div class="col-md-4" v-if="productForm.diachianh || productForm.anhgoc">
+        <label class="form-label d-block">Xem trước ảnh chính</label>
+        <!-- Nếu vừa chọn file mới (blob), ưu tiên hiện -->
+        <img v-if="productForm.diachianh && productForm.diachianh.startsWith('blob:')" :src="productForm.diachianh"
+          width="100" height="100" class="rounded" />
+        <!-- Nếu đã upload lên cloud hoặc load lại (link cloud) -->
+        <img v-else-if="productForm.anhgoc" :src="productForm.anhgoc" width="100" height="100" class="rounded" />
+      </div>
     </div>
 
     <!-- Buttons -->
     <div class="mt-4 d-flex justify-content-end gap-2">
-      <button type="button" class="btn btn-warning" @click="$emit('resetForm')">Làm Mới</button>
+      <button type="button" class="btn btn-warning " @click="$emit('resetForm')">Làm Mới</button>
       <button type="button" class="btn btn-primary fw-bold me-2" @click="handleCreate">
         Thêm
       </button>
-      <button type="button" class="btn btn-warning fw-bold" @click="handleUpdate">
+      <button type="button" class="btn btn-s  uccess fw-bold" @click="handleUpdate">
         Cập nhật
       </button>
       <button type="button" class="btn btn-danger" @click="$emit('deleteProduct')">Xóa</button>
